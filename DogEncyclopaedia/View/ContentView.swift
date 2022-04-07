c122bf563ba41e6728d5f2a0646b985978fed10c
RyanAu624 <ryanaulh@gmail.com>
2022-04-07 13:49:30 +0800

    Data update

--- DogEncyclopaedia/View/ContentView.swift
+++ DogEncyclopaedia/View/ContentView.swift
@@ -10,20 +10,20 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
+    
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
+    
     private var items: FetchedResults<Item>
     
-    
-
     var body: some View {
         NavigationView {
             List {
                 Image("HomeIcon")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .frame(width: 300, height: 80, alignment: .leading)
+                    .frame(width: 300, height: 80, alignment: .topLeading)
                 SearchBarView()
                     .padding(.top, 20)
                     .padding(.bottom)
@@ -31,17 +31,20 @@ struct ContentView: View {
                 Text("Variety List")
                     .fontWeight(.bold)
                     .font(.title)
-                ForEach  (dogs){ dog in
-                    NavigationLink {
-                        DogDetailView(dogImage: "\(dog.dogImage)", dogVariety: "\(dog.dogVariety)", dogVarietyDetail: "\(dog.dogVarietyDetail)")
-                    } label: {
+                ForEach (dogs) { dog in
+                    ZStack(alignment: .leading) {
+                        NavigationLink(destination: {
+                            DogDetailView(dogImage: "\(dog.dogImage)", dogVariety: "\(dog.dogVariety)", dogVarietyDetail: "\(dog.dogVarietyDetail)")
+                        }, label: {
+                            EmptyView()
+                        }).opacity(0)
                         CardView(dogImage: "\(dog.dogImage)", dogVariety: "\(dog.dogVariety)")
                     }
-                    
                 }
                 .listRowSeparator(.hidden)
             }
             .listStyle(PlainListStyle())
+            .navigationTitle("\(Image("HomeIcon"))")
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Button(action: {
--- DogEncyclopaedia/View/DogDetailView.swift
+++ DogEncyclopaedia/View/DogDetailView.swift
@@ -32,6 +32,6 @@ struct DogDetailView: View {
 
 struct DogDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DogDetailView(dogImage: "blankImage", dogVariety: "Dog Variety", dogVarietyDetail: "The Pekingese (also spelled Pekinese) is a breed of toy dog, originating in China. The breed was favored by royalty of the Chinese Imperial court as both a lap dog and companion dog, and its name refers to the city of Peking (Beijing) where the Forbidden City is located. The breed has several characteristics and health issues related to its unique appearance. Because of its desirable characteristics, the Pekingese has been part of the development of designer crossbreeds, such as the Peekapoo (crossed with a poodle) and Peke-a-tese (crossed with a Maltese).")
+        DogDetailView(dogImage: "blankImage", dogVariety: "Testing Data", dogVarietyDetail: "Testing Data")
     }
 }
