8b14235fa6c1cd69541ed9f572c695304f3d2bbe
Jacky Wong <chikonwong@gmail.com>
2022-04-08 13:09:52 +0800

    no message

--- DogEncyclopaedia/View/ContentView.swift
+++ DogEncyclopaedia/View/ContentView.swift
@@ -6,19 +6,35 @@
 //
 
 import SwiftUI
-import CoreData
+
+import CoreML
+import Vision
+import CoreImage
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
+    @ObservedObject var datas = ReadData()
     
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
+    @State var changeImage = false
+    @State var openCameraRoll = false
+    @State var imageSelected = UIImage()
     
-    private var items: FetchedResults<Item>
+    let model = DogEncyclopaediaClassifierModel()
+    
+    private func classifyImage(image:UIImage) {
+        let image = image
+        let resizedImage = image.resizeImageTo(size: CGSize(width: 299, height: 299))
+        let buffer = resizedImage!.convertToBuffer()
+        let output = try! model.prediction(image: buffer!)
+        let results = output.classLabelProbs.sorted { $0.1 > $1.1 }
+        let result = results[0].key
+        for dog in datas.dogs where dog.dog_name_en.lowercased() == result.lowercased(){
+            print(dog.id)
+            //                DogDetailView(dogImage: "\(dog.id)", dogVariety: "\(dog.dog_name_en)", dogVarietyDetail: "\(dog.description)")
+        }
+    }
     
     var body: some View {
-        NavigationView {
+        NavigationView{
             List {
                 Image("HomeIcon")
                     .resizable()
@@ -31,14 +47,14 @@ struct ContentView: View {
                 Text("Variety List")
                     .fontWeight(.bold)
                     .font(.title)
-                ForEach (dogs) { dog in
+                ForEach (datas.dogs) { dog in
                     ZStack(alignment: .leading) {
                         NavigationLink(destination: {
-                            DogDetailView(dogImage: "\(dog.dogImage)", dogVariety: "\(dog.dogVariety)", dogVarietyDetail: "\(dog.dogVarietyDetail)")
+                            DogDetailView(dogImage: "\(dog.id)", dogVariety: "\(dog.dog_name_en)", dogVarietyDetail: "\(dog.description)")
                         }, label: {
                             EmptyView()
                         }).opacity(0)
-                        CardView(dogImage: "\(dog.dogImage)", dogVariety: "\(dog.dogVariety)")
+                        CardView(dogImage: "\(dog.id)", dogVariety: "\(dog.dog_name_en)")
                     }
                 }
                 .listRowSeparator(.hidden)
@@ -48,18 +64,28 @@ struct ContentView: View {
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Button(action: {
+                        openCameraRoll = true
                         
                     }) {
                         Image(systemName: "camera")
                     }
                 }
             }
+        }.sheet(isPresented: $openCameraRoll){
+            ImagePicker(selectedImage: $imageSelected, sourceType: .photoLibrary)
+                .onDisappear{
+                    if $imageSelected != nil {
+                        classifyImage(image: imageSelected)
+                    }
+                }
         }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
+        
     }
 }
